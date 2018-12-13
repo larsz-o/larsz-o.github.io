3a4f12364c4e1052d14dbbc9c54061bd35622c19
Lars <larsmackenzie@gmail.com>
2018-12-13 17:41:49 -0600

    edited about me

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Image } from 'react-bootstrap';
-import Nav from '../Nav/Nav'; 
+import Nav from '../Nav/Nav';
 
 
 class Projects extends Component {
     render() {
         return (
-            <section className="jumbotron text-center white">
-                <Nav/>
-                <div className="container flex-box">
-                <h2>About Me</h2>
-                <p>I am a full-stack software developer with a background in research, higher education,
-                and social services.<br /> I love creating technology that can improve people's lives - from automating
-                tedious processes, to building platforms to connect people, to delivering meaningful insights from under-utilized data.</p>
-                    <h2>Technologies</h2>
+            <section>
+                <Nav />
+                <div className="container flex-box-cards">
+                    <h2>About Me</h2>
+                    <p>I am a full-stack software developer with a background in research, higher education, and social services. I love creating technology that can improve people's lives - from automating tedious processes, to building platforms to connect people, to delivering meaningful insights from under-utilized data.</p>
+                    <p>For the past six and a half years, I've been pursuing a PhD at the University of Minnesota, where I studied how software and data-driven decision-making impacts marginalized people.</p>
+                    <p>During this time, I was deeply involved with educational technology: helping faculty to integrate digital technology into their research and teaching, and training students to produce digital media projects. I've tackled big problems in this work, like figuring out how to archive hundreds of student-made video projects while protecting their privacy, or how to best highlight research insights from a qualitative study in a narrative digital map.</p>
+                    <p>I'm passionate about helping build software for people who need custom websites or web applications but who don't always have access to it -- small business owners, non-profit organizations, researchers, students, artists, and entreprenuers.</p>
                 </div>
             </section>
         );
