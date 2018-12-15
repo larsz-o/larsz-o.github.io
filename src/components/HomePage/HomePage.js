b555648fe9722de9effeaa6593a7c02b62f49808
Lars <larsmackenzie@gmail.com>
2018-12-14 18:52:08 -0600

    put homepage into grid

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, Button } from 'react-bootstrap';
+import { Image, Button, Grid, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux'; 
 import Nav from '../Nav/Nav'; 
 
@@ -9,18 +9,17 @@ class HomePage extends Component {
             <div>
                 <Nav/>
                     <section className="main">
-                        <div className="flex-box margin-top">
-                            <div className="col-md-4">
+                        <Grid>
+                            <Row>
+                            <Col xs={12} lg={6}>
                                 <Image src={require('../images/book.jpg')} alt="computer" height="400" rounded/>
-                            </div>
-                            <h2 className="col-md-4">Hello! I'm a full-stack software developer and I can help you build your professional or organizational web presence.</h2>
-                        </div>
-                        <div className="flex-box">
-                            <div className="col-md-4">
-                            </div>
-                            <div className="col-md-4 flex-box"><Button bsSize="large" bsStyle="info" onClick={()=>this.props.history.push('/services')}>Learn More</Button>
-                    </div>
-                        </div>
+                            </Col>
+                            <Col xs={12} lg={6}>
+                                <h2 className="col-md-4">Hello! I'm a full-stack software developer and I can help you build your professional or organizational web presence.</h2>
+                               <Button bsSize="large" bsStyle="info" onClick={()=>this.props.history.push('/services')}>Learn More</Button>
+                            </Col>
+                            </Row>
+                        </Grid>
                     </section>
            
             </div >
