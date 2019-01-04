1e2487f8fc5829d58888ccc032b0ad9630e2b626
Lars <larsmackenzie@gmail.com>
2019-01-04 11:16:08 -0600

    edited about page formatting

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -40,10 +40,13 @@ class Projects extends Component {
                             <Image src={require('./lars.jpg')} alt="Lars headshot" height="350" rounded responsive />
                         </Col>
                         <Col xs={12} md={6} lg={6}>
-                            <p>I am a full-stack software developer with a background in research, higher education, and social services. I love creating technology that can improve people's lives - from <span className="emphasis">automating tedious processes, to building platforms to connect people, to delivering meaningful insights from under-utilized data.</span></p>
-                            <p>For the past six and a half years, I've been pursuing a PhD at the University of Minnesota, where I studied how software and data-driven decision-making impacts marginalized people.</p>
-                            <p>During that time, I was deeply involved with educational technology: helping faculty to integrate digital technology into their research and teaching, and training students to produce digital media projects. I've tackled big problems in this work, like figuring out how to <span className="emphasis">archive hundreds of student-made video projects while protecting their privacy,</span> or how to best highlight research insights from qualitative studies in <span className="emphasis">narrative digital maps.</span></p>
-                            <p>I'm passionate about helping build software for people who need custom websites or web applications but who don't always have access to them -- small business owners, non-profit organizations, researchers, students, artists, and entreprenuers.</p>
+                            <p className="about-text">I am a full-stack software developer with a background in research, higher education, and social services. I love creating technology that can improve people's lives - from <span className="emphasis">automating tedious processes, to building platforms to connect people, to delivering meaningful insights from under-utilized data.</span></p>
+                            <div className="spacing"></div>
+                            <p className="about-text">For the past six years, I've been pursuing a PhD at the University of Minnesota, where I studied how software and data-driven decision-making impact marginalized people.</p>
+                            <div className="spacing"></div>
+                            <p className="about-text">During that time, I was deeply involved with educational technology: helping faculty to integrate digital technology into their research and teaching, and training students to produce digital media projects. I've tackled big problems in this work, like figuring out how to <span className="emphasis">archive hundreds of student-made video projects while protecting their privacy,</span> or how to best highlight research insights from qualitative studies in <span className="emphasis">narrative digital maps.</span></p>
+                            <div className="spacing"></div>
+                            <p className="about-text">I'm passionate about helping build software for people who need custom websites or web applications but who don't always have access to them -- small business owners, non-profit organizations, researchers, students, artists, and entreprenuers.</p>
                         </Col>
                     </Row>) : (<div><Row>
                         <Col xs={12} md={6} lg={6}>
