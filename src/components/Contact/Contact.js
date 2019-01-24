d9d59b94f9e352fe341c208f87ca253ab8349402
Lars <larsmackenzie@gmail.com>
2019-01-23 20:33:55 -0600

    pool config

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -108,9 +108,9 @@ class Contact extends Component {
                         <FormControl componentClass="textarea" rows={10} value={this.state.message} onChange={(event) => this.handleChangeFor(event, 'message')} required />
                     </FormGroup>
                     <div className="center">
-                        <div class="g-recaptcha" data-sitekey="6LeZTowUAAAAAIq7N6vqBAibgz4joIcpBT1NShel"></div>
                         <Button bsSize="large" type="submit" bsStyle="primary">Submit</Button>
                     </div>
+                    <div class="g-recaptcha" data-sitekey="6LeZTowUAAAAAIq7N6vqBAibgz4joIcpBT1NShel"></div>
                 </form>
                 ) : (<form onSubmit={(event) => this.handleSubmit(event)} className="full-width">
                     <h2 className="center">Let's connect!</h2>
@@ -136,6 +136,7 @@ class Contact extends Component {
                     <div className="center">
                         <Button bsSize="large" type="submit" bsStyle="primary">Submit</Button>
                     </div>
+                    <div class="g-recaptcha" data-sitekey="6LeZTowUAAAAAIq7N6vqBAibgz4joIcpBT1NShel"></div>
                 </form>)}
                 <Modal show={this.state.success} onHide={(event) => this.handleClose(event)}>
                     <Modal.Header>
