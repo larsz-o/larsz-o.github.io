9ca41d0fd76329805963adc5b33a1ac04f1782b7
Lars <larsmackenzie@gmail.com>
2019-02-02 12:49:10 -0600

    installed prerendering package to increase seo

--- server/server.js
+++ server/server.js
@@ -7,11 +7,13 @@ const bodyParser = require('body-parser');
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('prerender-node'));
 
 //Routes
 app.get('/.well-known/acme-challenge//.well-known/acme-challenge/Q3H3TlO3Ziyf4q-YafkYFYJRuTXKqg9nYb3bPq_ccF8.VjaiHBWQIqpCzLM13qdgpkm_QDC3R3fD_WV4AC2kFr4', (req, res)=>{
   res.send('Q3H3TlO3Ziyf4q-YafkYFYJRuTXKqg9nYb3bPq_ccF8.VjaiHBWQIqpCzLM13qdgpkm_QDC3R3fD_WV4AC2kFr4');
  })
+ // security certificate
 
 // Serve static files
 app.use(express.static('build'));
