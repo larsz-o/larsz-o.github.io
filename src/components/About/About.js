c96365b71195792ec3e84a19bc238006579e3618
Lars <larsmackenzie@gmail.com>
2019-09-04 15:58:30 -0400

    refactored to avoid duplicate code for mobile vs. desktop versions

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Image, Grid, Row, Col } from 'react-bootstrap';
-import Diamonds from '../Diamonds/Diamonds'; 
+import Diamonds from '../Diamonds/Diamonds';
 
 class Projects extends Component {
     constructor(props) {
@@ -33,13 +33,13 @@ class Projects extends Component {
                     <Row>
                         <div className="row-padding"></div>
                     </Row>
-                    {isDesktop ? (<Row>
+                    <Row>
                         <Col xs={12} md={6} lg={6}>
                             <Image className="no-borders" src={require('../images/lars.jpg')} alt="Lars headshot" height="350px" rounded />
                         </Col>
                         <Col xs={12} md={6} lg={6}>
-                            <ul className="about-text">
-                                <div><li class="list-border">I am a problem solver.</li></div>
+                            <ul className={`about-text-${isDesktop}`}>
+                                {isDesktop ? (<div><li class="list-border">I am a problem solver.</li></div>) : (<div className="margin-top"><li class="list-border">I am a problem solver.</li></div>)}
                                 <li>I currently design online educational experiences for <a href="https://www.linkedin.com/in/lars-mackenzie/" target="_blank" rel="noopener noreferrer">work.</a></li>
                                 <li>I get to do a lot of cool stuff like launch AWS environments for software engineering courses, build web apps, and consult with instructors on how to increase interactivity, collaboration, and peer learning in online learning environments.</li>
                                 <li>I care about accessibility, information architecture, and learning something new every day.</li>
@@ -50,34 +50,10 @@ class Projects extends Component {
                                 <li>I am training to make the perfect pizza</li>
                                 <li>I already have the perfect <a href="https://res.cloudinary.com/dnrk3irn4/image/upload/v1555811740/IMG_0366.jpg" target="_blank" rel="noopener noreferrer">dog.</a></li>
                             </ul>
+                            {!isDesktop && <div className="margin-top"><Diamonds /></div>}
                         </Col>
-                    </Row>) : (<div className="margin-bottom">
-                        
-                    <Row>
-                        <Col xs={12} md={12} lg={12} className="center">
-                            <Image className="no-borders" src="https://res.cloudinary.com/dnrk3irn4/image/upload/c_scale,h_350/v1548291363/lars.jpg" alt="Lars headshot" height="350" rounded responsive />
-                        </Col>
-                    </Row>
-                    <Col xs={12} md={6} lg={6}>
-                    
-                            <ul className="about-text-mobile">
-                                <div class="margin-top">
-                                <li class="list-border">I am a problem solver.</li></div>
-                                <li>I currently design online educational experiences for <a href="https://www.linkedin.com/in/lars-mackenzie/" target="_blank" rel="noopener noreferrer">work.</a></li>
-                                <li>I get to do a lot of cool stuff like launch AWS environments for software engineering courses, build web apps, and consult with instructors on how to increase interactivity, collaboration, and peer learning in online learning environments.</li>
-                                <li>I care about accessibility, information architecture, and learning something new every day.</li>
-                                <li>I have built some neat projects on my own and on teams as a <a href="https://www.github.com/larsz-o" target="_blank" rel="noopener noreferrer">full-stack software developer.</a></li>
-                                <li>I am trained as an academic and I wrote a dissertation about how <a href={require('../images/AfterlifeData.pdf')} target="_blank" rel="noopener noreferrer" title="Afterlife of Data">marginalized people navigate through commerical and medical information systems.</a></li>
-                                <li>I like reading <a href="https://twitter.com/larszmac" target="_blank" rel="noopener noreferrer">Twitter</a> but usually just delete my tweets.</li>
-                                <li>I look at my <a href="mailto:larsmackenzie@gmail.com?body=Hi Lars!">email</a> a lot and love to connect with new people.</li>
-                                <li>I am training to make the perfect pizza.</li>
-                                <li>I already have the perfect <a href="https://res.cloudinary.com/dnrk3irn4/image/upload/v1555811740/IMG_0366.jpg" target="_blank" rel="noopener noreferrer">dog.</a></li>
-                            </ul>
-                            <div className="margin-top"><Diamonds/></div>
-                        </Col>
-                        
-                       
-                        </div>)}
+                    </Row><div className="margin-bottom">
+                    </div>
                 </Grid>
 
             </section>
