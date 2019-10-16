d0776a481313a79e402d9b0d1170adeeebad5ac6
Lars <larsmackenzie@gmail.com>
2019-10-16 14:14:55 -0400

    added link to increase image size for project cards

--- src/components/ProjectView/ProjectView.js
+++ src/components/ProjectView/ProjectView.js
@@ -5,7 +5,7 @@ class ProjectView extends Component {
         return(
             <div className="column-4 project-card">
                 <h3>{this.props.project.title}</h3>
-                <img src={this.props.project.img} alt={this.props.project.img_desc}/>
+                <a href={this.props.project.img}><img src={this.props.project.img} alt={this.props.project.img_desc}/></a>
                 <p>{this.props.project.description}</p>
                {this.props.demo && <div className="center"><a href={this.props.project.url} target="_blank" rel="noopener noreferrer">View project</a></div>} 
             </div>
