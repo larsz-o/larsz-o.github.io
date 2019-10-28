ffe90fd004a58d1368ba18bb8f2d693abecaaaa4
Lars <larsmackenzie@gmail.com>
2019-10-28 10:10:35 -0400

    replace image, edit bio

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,13 @@ class Header extends Component {
             <div>
             <div className="row-padding"></div>
             <div className="flex-box-left">
-                <div className="column-1"></div>
+                <div className="column-2"></div>
                   <div className={`column-5 ${this.props.display}`}>
-                  <a href="/#/"><img src={require('../images/IMG_0968.JPG')} alt="Lars headshot" height="300px" className="bio-img"/></a>
+                  <a href="/#/"><img src={require('../images/DSC01757.jpg')} alt="Lars headshot" height="300px" className="bio-img"/></a>
                   </div>
                   <div className="column-5">
                   <h2 className="name-header">Lars Mackenzie, PhD</h2>
-                      <h3 className="subheading">full-stack web developer + designer</h3>
+                      <h3 className="subheading">web developer + designer</h3>
                       <div className={`flex-box ${this.props.display}`} >
                           
                       <a href="https://docs.google.com/document/d/13CpnBm968fYaO_TUW_baLE7okG6sEkg_Otq9w0qMEW0/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><div className="button"><h4>Resume</h4></div></a>
