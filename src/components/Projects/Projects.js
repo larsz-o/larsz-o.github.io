883c3d8f75c6eb5937fff97e9db72455a11e8225
Lars Mackenzie <larsmackenzie@gmail.com>
2019-12-03 11:11:36 -0500

    projects structure established

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,9 +7,9 @@ class Projects extends Component {
                      <div className="flex-box-left">
                     <div className="column-2"></div>
                     <div className="column-8 grayed">
-               <h3>Projects</h3>
-               <p>I've worked on a number of projects related to educational technology, teaching and research, and web development.</p>
-               
+               <h3>Projects and Skills</h3>
+              <p>My typical day involves working on a number of different projects concurrently. These are a few of the projects that I'm currently most excited about.</p>
+            {/* some nice layout of projects: digital storytelling, moodle cloud, garageband/slides, bookdown, pubpub, aws, github classrooms, topic selection tool, feminist pedagogy, digital ethics, health and wellness training, etc. */}
             </div>
             </div>
             </div>
