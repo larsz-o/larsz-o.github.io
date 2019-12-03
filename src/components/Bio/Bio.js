7b7d500e8ac090133a2688be84eb70f164514bfe
Lars Mackenzie <larsmackenzie@gmail.com>
2019-12-03 10:33:58 -0500

    edited structure

---
+++ src/components/Bio/Bio.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Bio extends Component {
+    render(){
+        return(
+            <div className="bio">
+                   <div className="flex-box-left">
+                    <div className="column-2"></div>
+                        <div className="column-8">      
+                             <h3 className="greeting">Hi, I'm Lars!</h3>
+                                <p>I'm a learning experience designer and full-stack web developer based in the Greater Boston area. I currently <a href="https://www.linkedin.com/in/lars-mackenzie/" target="_blank" rel="noopener noreferrer">work</a> at Brandeis University, where I build online educational experiences for the Rabb School of Continuing Studies, supporting faculty to foster interactivity, collaboration, and student success in digital classrooms.</p>   
+
+                               <p>I love exploring educational technology that promotes research and teaching. At Brandeis, I lead my team's efforts to support faculty who want to use cloud services for their teaching, such as Amazon Web Services and GitHub. I also support course development projects by developing web applications, editing audio and video, and creating interactive lessons. I believe that technology can transform the ways we learn, connect, create, and process information, but also that the learning goals should be the center of all of technology decisions.</p>
+
+                               <p>Prior to joining the team at Brandeis, I worked at the University of Minnesota, where I also completed my PhD. At Minnesota, I worked with faculty and graduate instructors to design and support digital projects in classroom instruction, aiding students in humanities and social science classrooms in developing digital stories and digital narrative maps. I also lead the production of online interactive modules for the Gender, Women, and Sexuality Studies department and coordinated the launch of a digital humanities initiative within that program. These projects all focused on integrating technology into teaching and research from a social justice perspective - thinking through how technology can give platforms to marginalized people and tell subjugated stories. This work also entailed carefully considering the ethics of technology production and attention to concerns around privacy and data sharing.</p>
+                               
+                    </div>
+                    </div>
+            </div>
+        )
+    }
+}
+export default Bio;
---
+++ src/components/Values/Values.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Values extends Component {
+    render(){
+        return(
+            <div>
+                <p>I am driven by my commitments to usability, critical thinking, scalability, and accessibility when developing student learning experiences. My </p> 
+
+<p> I'm interdisciplinary and collaborative at my core. I love working with teams across institutions to iteratively improve on processes or to rethink how things are done.</p>
+<p>I earned a doctorate from the University of Minnesota, where I explored the impacts of technology - <a href={require('../images/AfterlifeData.pdf')} target="_blank" rel="noopener noreferrer" title="Afterlife of Data">specifically, systems design</a> - on marginalized people. Before that, I earned a Bachelor's degree from Hampshire College, where I learned the power of experimental education first-hand.</p>
+<p>I've always been interested in how the best technology works to connect people and solve problems, and how poorly designed tech can have diasterous impacts on individuals, communities, and the world at large. During my doctoral program, I turned this interest towards analyzing information architecture, examining how data functions as a commodity that structures the lives of everyday people. </p>
+<p> I am on <a href="https://twitter.com/larszmac" target="_blank" rel="noopener noreferrer">Twitter,</a> but I usually just delete my tweets. In my spare time, I  am training to make the perfect pizza and working on building a robot for my freshwater aquarium. I already have the perfect <a href="https://res.cloudinary.com/dnrk3irn4/image/upload/v1555811740/IMG_0366.jpg" target="_blank" rel="noopener noreferrer">dog.</a></p>
+            </div>
+        )
+    }
+}
+export default Values; 
