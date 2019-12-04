7629d2f666117bf0b6b7e22ba97faf8ce7bbeff2
Lars <larsmackenzie@gmail.com>
2019-12-04 10:43:40 -0500

    added footer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,11 +3,13 @@ import Header from '../Header/Header';
 import Bio from '../Bio/Bio';
 import Projects from '../Projects/Projects';
 import Divider from '../Divider/Divider';
+import Footer from '../Footer/Footer';
 
 class About extends Component {
     render() {
 
         return (
+            <div>
             <div className="main">
                 <Header display={'true'} />
                 <div className="row-padding"></div>
@@ -16,8 +18,8 @@ class About extends Component {
                 <Divider />
                 <Projects/>
                 <div className="row-padding"></div>
-              
-
+            </div>
+            <Footer/>
             </div>
         );
     }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Footer extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            year: ''
+        }
+    }
+    componentDidMount(){
+        let year = new Date().getFullYear();
+        this.setState({
+            year: year
+        })
+
+    }
+    render(){
+        return(
+            <footer className="flex-box-between">
+                <p>Lars Mackenzie &copy;{this.state.year}</p>
+                <p><a href="https://github.com/larsz-o/larsz-o.github.io">page revision history</a></p>
+            </footer>
+        )
+    }
+}
+export default Footer;
