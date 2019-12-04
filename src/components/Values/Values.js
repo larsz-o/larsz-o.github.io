e2f2c6d05d1f3365c4c48076f617a7409bea81d2
Lars <larsmackenzie@gmail.com>
2019-12-04 07:56:53 -0500

    edit bio

--- src/components/Values/Values.js
+++ src/components/Values/Values.js
@@ -9,6 +9,9 @@ class Values extends Component {
                 <p>Open education, accessibility, critical pedagogy.</p>
                 <p>Ethical uses of technology and data</p>
                 <p>Universal design for learning</p>
+                <p>I have taught and designed my own courses, both online and in person. During my doctoral program, I received pedagogical training and completed extensive course work in disability studies and social justice frameworks.
+                I believe that technology can transform the ways we learn, connect, create, and process information, but also that learning goals should be at the center of all of technology decisions.
+                </p>
             </div>
         )
     }
