27b2cc2da74121a82bafa0089f0485ef14775208
Lars <larsmackenzie@gmail.com>
2020-02-19 10:20:18 -0500

    reduced image size

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'; 
 import About from './components/About/About'; 
 import FourOhFour from './components/404/404';
-import Portfolio from './components/Portfolio/Portfolio';
+
 
 class App extends Component {
   render() {
