7643946e9b7929d903f5d952b03d1b0616faf8b6
Lars Mackenzie <37813161+larsz-o@users.noreply.github.com>
2020-04-16 10:39:47 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import Social from '../Social/Social'; 
+import Social from '../Social/Social';
 
 class Header extends Component {
-    render(){
-        return(
-            <div className={`image-${this.props.display} header`}>
-            <div className="row-padding"></div>
-            <div className="flex-box-left">
-                <div className="column-1"></div>
-                 {this.props.display && <div className={`column-5`}>
-                  <a href="/#/"><img src={require('../images/DSC01757.jpg')} alt="Lars headshot" height="300px" className="bio-img"/></a>
-                  </div>}
-                 {this.props.display && <div className="column-1"></div> } 
-                  <div className="column-5">
-                  <a className="plain-link" href="/"><h2 className="name-header">Lars Mackenzie, PhD</h2></a>
-                      <h3 className="subheading">learning designer + web developer </h3>
-                      <div className={`flex-box flex-evenly flex-stretch column-12 ${this.props.display}`} >
-                      <Social/>
-                  </div>
-         
-                
-                  <div className="column-1"></div>
-                  </div>
-                  </div>
-                  </div>
+    render() {
+        return (
+            <div className="header">
+                <div className={`image-${this.props.display}`}>
+                    <div className="row-padding"></div>
+                    <div className="flex-box-left">
+                        <div className="column-1"></div>
+                        {this.props.display && <div className={`column-5`}>
+                            <a href="/#/"><img src={require('../images/lars-dark.png')} alt="Lars headshot" height="300px" className="bio-img" /></a>
+                        </div>}
+                        {this.props.display && <div className="column-1"></div>}
+                        <div className="column-5">
+                            <a className="plain-link" href="/"><h2 className="name-header">Lars Mackenzie, PhD</h2></a>
+                            <h3 className="subheading">learning designer + web developer </h3>
+                            <div className={`flex-box flex-evenly flex-stretch column-12 ${this.props.display}`} >
+                                <Social />
+                            </div>
+
+
+                            <div className="column-1"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         )
     }
 }
